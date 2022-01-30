8797f8b2d7d54da408f0c634f07540a24c665bbe
manatriton <manatriton.dev@gmail.com>
2022-01-30 15:36:59 -0800

    continue work on supporting expression types

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -11,6 +11,28 @@ pub enum LoxValue {
     String(String),
 }
 
+impl<'a> From<Literal<'a>> for LoxValue {
+    fn from(lit: Literal) -> Self {
+        match lit {
+            Literal::String(val) => LoxValue::String(val.to_string()),
+            Literal::Number(val) => LoxValue::Number(val),
+            Literal::Boolean(val) => LoxValue::Boolean(val),
+            Literal::None => LoxValue::Nil,
+        }
+    }
+}
+
+impl<'a, 'b> From<&'a Literal<'b>> for LoxValue {
+    fn from(lit: &'a Literal<'b>) -> Self {
+        match lit {
+            Literal::String(val) => LoxValue::String(val.to_string()),
+            Literal::Number(val) => LoxValue::Number(*val),
+            Literal::Boolean(val) => LoxValue::Boolean(*val),
+            Literal::None => LoxValue::Nil,
+        }
+    }
+}
+
 impl fmt::Display for LoxValue {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
@@ -57,17 +79,39 @@ impl Default for Interpreter {
     }
 }
 
+macro_rules! number {
+    ($val:ident) => {{
+        match $val {
+            LoxValue::Number(val) => val,
+            _ => return Err(InterpreterError::BadType),
+        }
+    }};
+}
+
 impl Visitor<Result<LoxValue>> for Interpreter {
     fn visit_expr(&mut self, expr: &Expr) -> Result<LoxValue> {
         match *expr {
-            Expr::Binary(ref lhs, ref op, ref rhs) => todo!(),
+            Expr::Binary(ref lhs, ref op, ref rhs) => {
+                let lhs = self.evaluate(lhs)?;
+                let rhs = self.evaluate(rhs)?;
+                match op.kind {
+                    TokenKind::Minus => Ok(LoxValue::Number(number!(lhs) - number!(rhs))),
+                    TokenKind::Slash => Ok(LoxValue::Number(number!(lhs) / number!(rhs))),
+                    TokenKind::Star => Ok(LoxValue::Number(number!(lhs) * number!(rhs))),
+                    TokenKind::Plus => match (lhs, rhs) {
+                        (LoxValue::Number(lhs), LoxValue::Number(rhs)) => {
+                            Ok(LoxValue::Number(lhs + rhs))
+                        }
+                        (LoxValue::String(lhs), LoxValue::String(rhs)) => {
+                            Ok(LoxValue::String(lhs + &rhs))
+                        }
+                        _ => Err(InterpreterError::BadType),
+                    },
+                    _ => Err(InterpreterError::BadOperator),
+                }
+            }
             Expr::Grouping(ref expr) => self.visit_expr(expr),
-            Expr::Literal(ref lit) => match *lit {
-                Literal::String(s) => Ok(LoxValue::String(s.to_string())),
-                Literal::Number(n) => Ok(LoxValue::Number(n)),
-                Literal::Boolean(b) => Ok(LoxValue::Boolean(b)),
-                Literal::None => Ok(LoxValue::Nil),
-            },
+            Expr::Literal(ref lit) => Ok(LoxValue::from(lit)),
             Expr::Unary(ref op, ref rhs) => match op.kind {
                 TokenKind::Minus => match self.visit_expr(rhs)? {
                     LoxValue::Number(val) => Ok(LoxValue::Number(-val)),
@@ -76,11 +120,20 @@ impl Visitor<Result<LoxValue>> for Interpreter {
                 TokenKind::Bang => Ok(LoxValue::Boolean(!is_truthy(&self.visit_expr(rhs)?))),
                 _ => Err(InterpreterError::BadOperator),
             },
-            Expr::Ternary(ref lhs, ref op1, ref mhs, ref op2, ref rhs) => todo!(),
+            Expr::Ternary(ref lhs, ref op1, ref mhs, ref op2, ref rhs) => {
+                match (op1.kind, op2.kind) {
+                    (TokenKind::Question, TokenKind::Colon) => {
+                        let lhs = self.evaluate(lhs)?;
+                        self.evaluate(if is_truthy(&lhs) { mhs } else { rhs })
+                    }
+                    _ => Err(InterpreterError::BadOperator),
+                }
+            }
         }
     }
 }
 
+#[inline]
 fn is_truthy(val: &LoxValue) -> bool {
     match val {
         LoxValue::Object => todo!(),
